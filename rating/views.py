baf25335648229bfa2515a88f501f19e2ab95d6b
Lev2307 <levchik23072007@gmail.com>
2021-11-26 17:39:10 +0300

    upd

--- rating/views.py
+++ rating/views.py
@@ -5,20 +5,35 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 from .models import Rating, Subject
 from django.views.generic.edit import FormMixin
 from .forms import RateForm
 from django.urls import reverse
+from django.views.generic.detail import SingleObjectMixin
 
 class RatingsDetailView(FormMixin, DetailView):
     model = Subject
-    template_name = 'rating/rating_detail.html'
     form_class = RateForm
+    template_name = 'rating/rating_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = RateForm()
+        return context
 
     def get_success_url(self):
         return reverse('main')
 
+
+class RatingsDetailFormView(SingleObjectMixin, FormView):
+    model = Subject
+    form_class = RateForm
+    template_name = 'rating/rating_detail.html'
+    
+    def get_success_url(self):
+        return reverse('main')
+    
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         form = self.get_form()
@@ -49,6 +64,17 @@ class RatingsEntryListView(ListView):
 
     def get_queryset(self):
         return Subject.objects.filter(name=self.kwargs['name'])
+
+
+class RatingView(View):
+
+    def get(self, request, *args, **kwargs):
+        view = RatingsDetailView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = RatingsDetailFormView.as_view()
+        return view(request, *args, **kwargs)
 
 class SimpleFormView(View):
     form_class = SimpleForm
